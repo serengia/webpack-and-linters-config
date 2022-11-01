68f21f56af94fdef52c54f3afa27f0704d3f7b0d
James Serengia <jamesserengia@gmail.com>
2022-11-01 19:30:24 +0300

    Loaded css styles with webpack and setup webpack server

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,30 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  devServer: {
+    static: "./dist",
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
     }),
   ],
+  mode: "development",
+  //   optimization: {
+  //     runtimeChunk: "single",
+  //   },
+
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
 };
